45c2c530164d9799b2c1e38e221bc553d075576f
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2022-09-25 16:09:12 +0900

    Nickname 및 Room 상태 알림 추가
    - socket별 nickname 설정
    - adapter + js Maps를 이용한 SocketIO 서버 내의 Room 목록 관리 (서버 접속, Room 생성, Room 연결 해제 시 전체 Room 목록 재설정)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,14 @@
 const socket = io(); // 알아서 io 실행 중인 서버를 찾아서 연결
 
 const welcome = document.getElementById("welcome");
-const form = document.querySelector("form");
+const form = document.querySelector("#enterRoom");
 const room = document.getElementById("room");
 
 // Room 접속 폼만 보이기
 room.hidden = true;
 
 let roomName;
+let nickName;
 
 // Message 출력 함수
 const addMessage = (msg) => {
@@ -17,6 +18,17 @@ const addMessage = (msg) => {
     ul.appendChild(li);
 };
 
+// Room 채팅 출력 함수
+const handleMessageSubmit = (event) => {
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const msg = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${msg}`);
+    });
+    input.value = "";
+};
+
 // Room 접속 폼 => Room 내 메시지 전송 폼 전환
 const showRoom = () => {
     welcome.hidden = true;
@@ -24,22 +36,54 @@ const showRoom = () => {
     // setting room name
     const h3 = room.querySelector("h3");
     h3.innerText = `[Room] ${roomName}`; // socket.emit은 비동기 처리로 roomName이 먼저 설정돼서 가능
+    // setting nickname
+    const h4 = room.querySelector("h4");
+    h4.innerText = `[My Nickname] ${nickName}`;
+    // Room 내 채팅 이벤트 추가
+    const msgForm = room.querySelector("#msg");
+    msgForm.addEventListener("submit", handleMessageSubmit);
 };
 
-// room join handler
+// Room 입장 핸들러
 const handleRoomSubmit = (event) => {
     event.preventDefault();
-    const input = form.querySelector("input");
+    const inputRoom = form.querySelector("#roomName");
+    const inputNickname = form.querySelector("#nickName");
 
-    // send message
-    socket.emit("enter_room", input.value, showRoom);
-    roomName = input.value;
-    input.value = "";
+    // Room, Nickname 설정 후 입장
+    roomName = inputRoom.value;
+    nickName = inputNickname.value;
+    socket.emit("enter_room", roomName, nickName, showRoom);
+
+    inputRoom.value = "";
+    inputNickname.value = "";
 };
 
 form.addEventListener("submit", handleRoomSubmit);
 
-// Room 내 유저 입장 체크
-socket.on("welcome", () => {
-    addMessage("Someone joined!");
+// Room 내 유저 입장 체크 (welcome event )
+socket.on("welcome", (user) => {
+    addMessage(`${user} joined!`);
+});
+
+// Room 내 유저 퇴장 체크 (bye event)
+socket.on("bye", (user) => {
+    addMessage(`${user} left...`);
+});
+
+// Room 채팅 메시지 체크 (new_message event)
+socket.on("new_message", addMessage); // addMessage = (msg) => {addMessage(msg)}
+
+socket.on("room_change", (rooms) => {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = ""; // Room 리스트 중복 출력 방지를 위해 빈 값 초기화
+    if (rooms.length === 0) {
+        return;
+    }
+
+    rooms.forEach((room) => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    });
 });
--- src/server.js
+++ src/server.js
@@ -23,17 +23,59 @@ const httpServer = http.createServer(app);
 // socket.io 서버
 const wsServer = SocketIO(httpServer);
 
+// Public Room 목록
+const publicRoom = () => {
+    const {
+        sockets: {
+            adapter: { sids, rooms },
+        },
+    } = wsServer;
+    const publicRooms = [];
+
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+
+    return publicRooms;
+};
+
 // socket.io connection
 wsServer.on("connection", (socket) => {
+    // 현재 생성된 Room 목록 알림
+    wsServer.sockets.emit("room_change", publicRoom());
     // socket.onAny: Socket에 있는 모든 event 체크
     socket.onAny((event) => {
         console.log(`Socket Events: ${event}`);
     });
-
-    socket.on("enter_room", (roomName, done) => {
+    // Room 입장 시 이벤트
+    socket.on("enter_room", (roomName, nickName, done) => {
+        socket["nickname"] = nickName;
         socket.join(roomName); // room 생성 후 join
         done();
-        socket.to(roomName).emit("welcome"); // send message in the room (Except Me)
+        // welcome event
+        socket.to(roomName).emit("welcome", socket.nickname); // send message in the room (Except Me)
+        // Room 변경사항 전체 알림
+        wsServer.sockets.emit("room_change", publicRoom());
+    });
+    // Room 퇴장 시 이벤트
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => {
+            // bye event
+            socket.to(room).emit("bye", socket.nickname);
+        });
+    });
+    // Room 연결 해제 이벤트
+    socket.on("disconnect", () => {
+        // Room 변경사항 전체 알림
+        wsServer.sockets.emit("room_change", publicRoom());
+    });
+
+    // Room 내 채팅 발생 이벤트
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
     });
 });
 
